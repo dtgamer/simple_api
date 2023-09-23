b36448d15ac2af0951c21dfe7684b97a4abdab9b
dtgamer <144635041+dtgamer@users.noreply.github.com>
2023-09-23 01:57:24 +0100

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.views import View
-from rest_framework import serializers
 from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
+from rest_framework import status
+from django.contrib.auth.hashers import make_password, check_password  # For password hashing
 from .models import User
-
-class UserSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=255)
+from .serializers import UserSerializer  # Import your serializer
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UserView(View):
@@ -22,16 +17,22 @@ class UserView(View):
 
             if serializer.is_valid():
                 user_data = serializer.validated_data
-                user, created = User.objects.get_or_create(email=user_data['email'], defaults=user_data)
-                if not created:
-                    return JsonResponse({'error': 'User with this email already exists.'}, status=400)
 
-                return JsonResponse({'message': 'User created successfully.'}, status=201)
+                # Hash the password before saving it
+                user_data['password'] = make_password(user_data['password'])
+
+                # Create or get the user by email
+                user, created = User.objects.get_or_create(email=user_data['email'], defaults=user_data)
+                
+                if not created:
+                    return JsonResponse({'error': 'User with this email already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+
+                return JsonResponse({'message': 'User created successfully.'}, status=status.HTTP_201_CREATED)
             else:
-                return JsonResponse({'errors': serializer.errors}, status=400)
+                return JsonResponse({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
         except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
+            return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def get(self, request):
         try:
@@ -39,15 +40,22 @@ class UserView(View):
             password = request.GET.get('password', '')
 
             if not email or not password:
-                return JsonResponse({'error': 'Email and password are required.'}, status=400)
+                return JsonResponse({'error': 'Email and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
 
             try:
-                user = User.objects.get(email=email, password=password)
-                serializer = UserSerializer(user)
-                return JsonResponse(serializer.data)
+                user = User.objects.get(email=email)
+
+                # Check the password using Django's check_password function
+                if check_password(password, user.password):
+                    serializer = UserSerializer(user)
+                    return JsonResponse(serializer.data)
+                else:
+                    return JsonResponse({'error': 'Invalid password.'}, status=status.HTTP_401_UNAUTHORIZED)
+
             except User.DoesNotExist:
-                return JsonResponse({'error': 'User not found.'}, status=404)
+                return JsonResponse({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
+            return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
